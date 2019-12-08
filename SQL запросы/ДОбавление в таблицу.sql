b48eff3f7aef82393390b8fdf271242f4942c01d
NastyaSab <nastya-xd@list,ru>
2019-12-08 18:20:27 +0300

    ДОбавили SQL запросы

---
+++ SQL запросы/ДОбавление в таблицу.sql
@@ -0,0 +1,2 @@
+INSERT INTO dbo.Persons (PersonID, FirstName, LastName, City, Address)
+VALUES (1, 'Настя', 'Сабурова', 'Москва', 'Солнцево');
---
+++ SQL запросы/Создание таблицы.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Persons (
+    PersonID int, /* int - целые числа */
+    LastName varchar(255),  /* varchar - числа, буквы, символы */
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
