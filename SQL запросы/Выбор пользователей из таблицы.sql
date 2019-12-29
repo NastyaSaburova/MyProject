7d096d6ddda9105004126f0c3855f2445f100f75
NastyaSab <nastya-xd@list,ru>
2019-12-29 17:45:59 +0300

    Добавили продукты, компьютеры. Изучили Distinct, in, where, between

--- SQL запросы/Выбор пользователей из таблицы.sql
+++ SQL запросы/Выбор пользователей из таблицы.sql
@@ -1,1 +1,33 @@
 select * from dbo.Persons
+
+select * 
+from PC;
+
+select model, speed, hd 
+from PC
+where price < 400;
+
+select * 
+from product;
+
+select product.maker, pc.speed, pc.hd
+from product, pc
+where product.model = pc.model and pc.hd > 45
+
+select distinct maker from Product
+
+select ram from pc where ram < 45
+
+select * from pc
+where ram in (select ram from pc where ram < 45)
+
+select * from pc
+where price > '45s'
+
+select * 
+from PC
+where ram >= 15 and ram <= 45;
+
+select * 
+from PC
+where ram between 15 and 45;
--- SQL запросы/Создание таблицы.sql
+++ SQL запросы/Создание таблицы.sql
@@ -1,7 +1,50 @@
+﻿/* Создание таблицы Persons */
 CREATE TABLE Persons (
-    PersonID int, /* int - ����� ����� */
-    LastName varchar(255),  /* varchar - �����, �����, ������� */
+    PersonID int, /* int - целые числа */
+    LastName varchar(255),  /* varchar - числа, буквы, символы */
     FirstName varchar(255),
     Address varchar(255),
     City varchar(255)
+);
+
+/* Создание таблицы Product */
+CREATE TABLE Product (
+    maker varchar(10) NOT NULL, /* int - целые числа */
+	model varchar(50) NOT NULL,
+	type varchar(50) NOT NULL,
+	PRIMARY KEY (model)
+);
+
+/* Создание таблицы PC */
+CREATE TABLE PC (
+	code int not null,
+	model varchar(50) references Product(model) not null,
+	speed smallint not null,
+	ram smallint not null,
+	hd real not null,
+	cd varchar(10) not null,
+	price money null,
+	PRIMARY KEY (code)
+);
+
+/* Создание таблицы Laptop */
+CREATE TABLE Laptop (
+	code int not null,
+	model varchar(50) references Product(model) not null,
+	speed smallint not null,
+	ram smallint not null,
+	hd real not null,
+	screen tinyint not null,
+	price money null,
+	PRIMARY KEY (code)
+);
+
+/* Создание таблицы Printer */
+CREATE TABLE Printer (
+	code int not null,
+	model varchar(50) references Product(model) not null,
+	type varchar(10) not null,
+	color char(10) not null,
+	price money null,
+	PRIMARY KEY (code)
 );
